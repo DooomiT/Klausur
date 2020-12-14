ccf814bef8c722079be38668ed349146806278b3
Florian Bufler <florian.bufler@gmx.de>
2020-12-14 19:58:57 +0100

    implemented partially (todo: fix creating db user)

--- mongo-init.sh
+++ mongo-init.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-set -e
+mongo -- "$MONGO_INITDB_DATABASE" 
 mongo <<EOF
+use admin
+db.createUser({
+  user:  '$MONGO_INITDB_ROOT_USERNAME',
+  pwd: '$MONGO_INITDB_ROOT_PASSWORD',
+  roles: [{
+    role: 'root',
+    db: 'admin'
+  }]
+})
+
+use '$MONGO_INITDB_DATABASE'
 db.createUser({
   user:  '$MONGO_INITDB_USERNAME',
   pwd: '$MONGO_INITDB_PASSWORD',
