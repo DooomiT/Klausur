529082b13d76f037bbd8a62bfc8ff89eaab1fade
Florian Bufler <florian.bufler@gmx.de>
2020-12-14 14:30:34 +0100

    tmp

--- mongo-init.js
+++ mongo-init.js
@@ -1,12 +1,12 @@
 
 db.createUser(
     {
-        user: "<" + process.env.MONGO_INITDB_USERNAME + ">",
-        pwd: "<" + process.env.MONGO_INITDB_PASSWORD + ">",
+        user: process.env.MONGO_INITDB_USERNAME,
+        pwd: process.env.MONGO_INITDB_PASSWORD,
         roles: [
             {
                 role: "readWrite",
-                db: "<" + process.env.MONGO_INITDB_DATABASE + ">"
+                db: process.env.MONGO_INITDB_DATABASE
             }
         ]
     }
