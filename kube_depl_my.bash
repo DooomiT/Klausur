f53c749cefdcd32957187687459b37002a76d3d7
Florian Bufler <florian.bufler@gmx.de>
2020-12-15 21:30:40 +0100

    fixed autorun

--- kube_depl_my.bash
+++ kube_depl_my.bash
@@ -9,3 +9,5 @@ kubectl apply -f dhbw-seminarbeitWS2020-receivedata/manifests/dhbw-seminarbeit-r
 kubectl apply -f dhbw-seminarbeitWS2020-receivedata/manifests/dhbw-seminarbeit-receivedata-service.yaml
 kubectl apply -f dhbw-seminarbeitWS2020-eventbus/manifests/dhbw-seminarbeit-eventbus-deployment.yaml
 kubectl apply -f dhbw-seminarbeitWS2020-displaydata/manifests/dhbw-seminarbeit-displaydata-deployment.yaml
+kubectl apply -f dhbw-seminarbeitWS2020-displaydata/manifests/dhbw-seminarbeit-service.yaml
+
